ef11833d842c305e403656d6115c8455f4ed40fc
Kaleb-EA <102551602+Kaleb-EA@users.noreply.github.com>
2023-04-26 14:45:53 -0400

    Many updates to display. Commit is a bit messy

---
+++ Testing/Testing.js
@@ -0,0 +1,196 @@
+/* eslint-disable max-len */
+
+/*
+  Hook this script to index.html
+  by adding `<script src="script.js">` just before your closing `</body>` tag
+*/
+
+/*
+  ## Utility Functions
+    Under this comment place any utility functions you need - like an inclusive random number selector
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+*/
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+function injectHTML(list) {
+  console.log('fired injectHTML')
+  const target = document.querySelector('#restaurant_list');
+  target.innerHTML = '';
+  list.forEach((item) => {
+    const str = `<li>${item.name}</li>`;
+    target.innerHTML += str
+  })
+}
+function filterList(list, query) {
+  return list.filter((item) => {
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseQuery = query.toLowerCase();
+    return lowerCaseName.includes(lowerCaseQuery);
+  });
+}
+function cutRestaurantList(list) {
+  console.log("fired cut list")
+  const range = [...Array(15).keys()];
+  return newArray = range.map((item) => {
+    const index = getRandomIntInclusive(0, list.length -1);
+    return list[index]
+  })
+}
+
+function initMap(){
+  var carto = L.map('map').setView([38.98, -76.93], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(carto);
+ return carto;
+}
+
+//function markerPlace(array, map) {
+  //console.log('array for markers', array);
+
+  //map.eachLayer((layer) => {
+    //if (layer instanceof L.Marker) {
+      //layer.remove();
+    //}
+  //});
+
+ // array.forEach((item) => {
+    //console.log('markerPlace', item);
+    //const {coordinates} = item.geocoded_column_1;
+
+    //L.marker([coordinates[1], coordinates[0]]).addTo(map);
+  //})
+
+function intitChart() {
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+  return new Chart(
+    chart,
+    config
+  );
+}
+
+function shapeDataforChart(array) {
+  return array.reduce((collection, item) => {
+    if(!collection[item.category]) {
+      collection[item.category] = [item]
+    } else{
+      collection[item.category].push(item);
+    }
+    return collection;
+  }, {});
+}
+
+
+async function getData() {
+  const url = 'https://www.superheroapi.com/api.php/1952847071749817/69/powerstats';
+      const data = await fetch(url);
+      const json = await data.json();
+      if(data.response === 'success'){
+        console.log(data);
+        showSearchList(data.results);
+      }
+      const obj = json.parse(data);
+      const reply = json.filter((item) => Boolean(item.geocoded_column_1)).fileter((item) => Boolean(item.name));
+      return reply;
+}
+
+async function mainEvent() { // the async keyword means we can make API requests
+  const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
+  //const filterButton = document.querySelector('#filter');
+  const loadDataButton = document.querySelector('#data_load');
+  const clearDataButton = document.querySelector('#data_clear');
+  const generateListButton = document.querySelector('#generate');
+  const textfield = document.querySelector('#resto')
+
+  const loadAnimation = document.querySelector('#data_load_animation')
+  const chartTarget = document.querySelector ('#myChart');
+  loadAnimation.style.display = "none";
+  generateListButton.classList.add("hidden"); 
+
+  const chartData = await getData();
+  const shapedData = shapeDataforChart(chartData);
+  const myChart = intitChart(chartTarget, shapedData);
+  
+
+  //const carto = initMap();  
+
+  const storedData = localStorage.getItem("storedData");
+  let parsedData = JSON.parse(storedData);
+  if (chartData.length > 0) {
+    generateListButton.classList.remove("hidden");
+    
+  }
+  let currentList = [];
+
+  loadDataButton.addEventListener('click', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
+    console.log('Loading data'); // this is substituting for a "breakpoint"
+    loadAnimation.style.display = "inline-block"
+      
+   
+      
+    const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    
+    const storedList = await results.json();
+    localStorage.setItem("storedData", JSON.stringify(storedList));
+    parsedData = storedList;
+    if (storedList?.length > 0) {
+      generateListButton.classList.remove("hidden");
+    }
+    loadAnimation.style.display = "none";
+    //console.table(storedList); 
+  });
+
+
+  generateListButton.addEventListener("click", (event) => {
+      console.log("generate new list");
+      currentList = cutRestaurantList(parsedData);
+      console.log(currentList);
+      injectHTML(currentList);
+      //markerPlace(currentList, carto);
+  });
+
+  textfield.addEventListener('input', (event) => {
+      console.log('input', event.target.value);
+      const newList = filterList(currentList, event.target.value);
+      console.log(newList);
+      injectHTML(newList);
+      //markerPlace(newList, carto);
+
+  })
+
+  clearDataButton.addEventListener('click', (event) =>{
+    console.log('clear browser data');
+    localStorage.clear();
+    console.log('local storage check', localStorage.getItem("storedData"))
+  })
+}
+
+/*
+  This adds an event listener that fires our main event only once our page elements have loaded
+  The use of the async keyword means we can "await" events before continuing in our scripts
+  In this case, we load some data when the form has submitted
+*/
+document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
--- script.js
+++ script.js
@@ -74,3 +74,81 @@ document.querySelector('.prev') // Get the appropriate element (<button class="n
     console.log('clicked prev'); // let's tell the client console we made it to this point in the script
     moveToPrevSlide(); // call the function above to handle this
   });
+
+// constanstns and event listener for character search
+  const searchBtn = document.getElementById('search-btn');
+  searchBtn.addEventListener('click', () => {
+    const heroId = document.getElementById('hero-id').value;
+  
+    // Fetching and displaying characters powerstats data
+    fetch(`https://www.superheroapi.com/api.php/1952847071749817/${heroId}/powerstats`)
+    .then(response => response.json())
+    .then(data => {
+      const labels = ['Intelligence', 'Strength', 'Speed', 'Durability', 'Power', 'Combat'];
+      const values = [
+        data.intelligence,
+        data.strength,
+        data.speed,
+        data.durability,
+        data.power,
+        data.combat
+      ];
+
+      const ctx = document.getElementById('power-stats').getContext('2d');
+      const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: labels,
+          datasets: [{
+            label: 'Powerstats',
+            data: values,
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              }
+            }]
+          }
+        }
+      });
+    })
+    .catch(error => console.log(error));
+  
+    // Fetching and displaying heroes biography data
+    fetch(`https://www.superheroapi.com/api.php/1952847071749817/${heroId}/biography`)
+      .then(response => response.json())
+      .then(data => {
+        const biographyList = document.getElementById('biography');
+        biographyList.innerHTML = `
+          <li><strong>Name:</strong> ${data['full-name']}</li>
+          <li><strong>Alter Egos:</strong> ${data['alter-egos']}</li>
+          <li><strong>Aliases:</strong> ${data.aliases.join(', ')}</li>
+          <li><strong>Place of Birth:</strong> ${data['place-of-birth']}</li>
+          <li><strong>First Appearance:</strong> ${data['first-appearance']}</li>
+          <li><strong>Publisher:</strong> ${data.publisher}</li>
+          <li><strong>Alignment:</strong> ${data.alignment}</li>
+          `;
+          document.getElementById('hero-id').value = '';
+        })
+        .catch(error => console.error(error));
+    });
