2e9f7cf253a5b01d02c31b94492637c32491b512
Kaleb-EA <102551602+Kaleb-EA@users.noreply.github.com>
2023-05-09 12:07:21 -0400

    Many updates to site

--- script.js
+++ script.js
@@ -1,80 +1,3 @@
-/* eslint-disable max-len */
-/*
-  Welcome to Javascript!
-
-  This file contains parts of a simple script to make your carousel work.
-  Please feel free to edit away - the main version of this with all the notes is safely stored elsewhere
-*/
-/* eslint-enable max-len */
-// set our first slide's position to "0", the opening position in an array
-let slidePosition = 0;
-
-// gather a reference to every slide we're using via the class name and querySelectorAll
-const slides = document.querySelectorAll('.carousel_item');
-
-// change that "NodeList" into a Javascript "array", to get access to "array methods"
-const slidesArray = Array.from(slides);
-
-// Figure out how many slides we have available
-const totalSlides = slidesArray.length;
-
-function updateSlidePosition() {
-  slidesArray.forEach(slide => {
-    slide.classList.remove('visible');  
-    slide.classList.add('hidden');  
-  });
-
-  slides[slidePosition].classList.add('visible');
-}
-
-function moveToNextSlide() {
-  /*
-    add an if statement here that checks
-    if you're already at the max number of slides
-    and if so, sets your slidePosition to the first index of an array
-    if not, set the slidePosition to the current position plus one
-  */
-  if(slidePosition === totalSlides - 1){
-    slidePosition = 0;
-  } else { 
-    slidePosition += 1;
-}
-  updateSlidePosition(); // this is how you call a function within a function
-}
-
-function moveToPrevSlide() {
-  /*
-    add an if statement here that checks
-    if you're already at the first index position for an array
-    and if so, sets your slidePosition to the last slide position in totalSlides
-    if not, set the slidePosition to the current position minus one
-  */
-  if(slidePosition === 0){
-    slidePosition = totalSlides - 1;
-  } else { 
-    slidePosition -= 1;
-} 
-  updateSlidePosition();
-}
-
-/*
-  These two functions have been assigned via "addEventListener"
-  to the elements accessed by the "querySelector" set to the class name on each
-*/
-document.querySelector('.next') // Get the appropriate element (<button class="next">)
-  .addEventListener('click', () => { // set an event listener on it - when it's clicked, do this callback function
-    console.log('clicked next'); // let's tell the client console we made it to this point in the script
-    moveToNextSlide(); // call the function above to handle this
-  });
-
-// Paying close attention to the above queryselector, write one that fires
-// when you want a "prev" slide
-document.querySelector('.prev') // Get the appropriate element (<button class="next">)
-  .addEventListener('click', () => { // set an event listener on it - when it's clicked, do this callback function
-    console.log('clicked prev'); // let's tell the client console we made it to this point in the script
-    moveToPrevSlide(); // call the function above to handle this
-  });
-  
   let chartInstance;
 
   // Function that displays powerstats as a chart
@@ -148,29 +71,41 @@ const displayBiography = (biography) => {
     <li><strong>Alignment:</strong> ${biography.alignment}</li>
   `;
 };
+
+
+// Function for displaying searched hero related comics
+const imagesFolder = './images/';
+const img = document.getElementById('my-image');
+img.alt = 'No movie information available'; // alt text
+
 const fetchHeroData = async (name) => {
   try {
-    console.log('API call started');
     const response = await fetch(`https://www.superheroapi.com/api.php/1952847071749817/search/${name}`);
     const data = await response.json();
-    console.log('API call successful');
-    return data.results[0];
+    const result = data.results.filter(hero => hero.name.toLowerCase() === name.toLowerCase())[0];
+    return result;
   } catch (error) {
     console.error(error);
   }
 };
 
-// Function that reloads powerstats chart and biography
-const reloadPowerstatsAndBiography = async (heroName) => {
-  // Fetches for displaying hero's powerstats data
+const displayImage = async (heroName) => {
+  const heroData = await fetchHeroData(heroName);
+  const imageName = heroData.name.toLowerCase().replace(/\s/g, '-') + '.png';
+  img.src = imagesFolder + imageName;
+};
+
+// Function that reloads powerstats chart, biography and image
+const reloadAllData = async (heroName) => {
+  // Reload powerstats and biography for the current search
   const heroData = await fetchHeroData(heroName);
   const powerstats = heroData.powerstats;
+  const biography = heroData.biography;
   displayPowerstats(powerstats);
-
-  //Function that fetchs for displaying hero's biography data
-  const bioData = await fetchHeroData(heroName);
-  const biography = bioData.biography;
   displayBiography(biography);
+
+  // Funtion to display the searched hero related image
+  await displayImage(heroName);
 };
 
 // Event listener for the search button
@@ -178,18 +113,19 @@ const searchBtn = document.getElementById('search-btn');
 searchBtn.addEventListener('click', async () => {
   const heroName = document.getElementById('hero-name').value;
 
-  // Check if previous searches are stored in local storage
+  // Function to check if previous searches are stored in local storage
   let previousSearches = JSON.parse(localStorage.getItem('previousSearches')) || [];
 
-  // Add current search to the array of previous searches
+  // Function that adds current search to the array of previous searches
   previousSearches.push(heroName);
 
-  // Store the updated array of previous searches in local storage
+  // Function that stores the updated array of previous searches in local storage
   localStorage.setItem('previousSearches', JSON.stringify(previousSearches));
 
-  // Reload powerstats and biography for the current search
-  await reloadPowerstatsAndBiography(heroName);
+  // Function that reloads all data for the current search
+  await reloadAllData(heroName);
 });
+
 
 // Event listener for the reset button
 const resetButton = document.getElementById('reset-button');
@@ -199,14 +135,14 @@ resetButton.addEventListener('click', () => {
     chartInstance.destroy();
   }
 
-  // Function to clear the biography list
+  // Function that clears the biography list
   const biographyList = document.getElementById('biography');
   biographyList.innerHTML = '';
 
-  // Function to clear the hero name input field
+  // Function tbat clears the hero name input field
   document.getElementById('hero-name').value = '';
 
-  // Function to display previous searches
+  // Function that displays previous searches
   const previousSearches = JSON.parse(localStorage.getItem('previousSearches')) || [];
   const previousSearchesList = document.getElementById('previous-searches');
   previousSearchesList.innerHTML = '';
@@ -218,11 +154,11 @@ resetButton.addEventListener('click', () => {
   });
 });
 
-  // Function to clear the biography list
+  // Function that clears the biography list
   const biographyList = document.getElementById('biography');
   biographyList.innerHTML = '';
 
-  // Function to clear the hero name input field
+  // Function that clears the hero name input field
   document.getElementById('hero-name').value = '';
 
 
